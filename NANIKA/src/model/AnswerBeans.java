bd7bbee98bc644a894d4f769788eadeba6fa4e0c
dojo2021-m-muraoka <muraoka@seplus2016.onmicrosoft.com>
2021-06-15 17:36:08 +0900

    ^^

--- NANIKA/src/model/AnswerBeans.java
+++ NANIKA/src/model/AnswerBeans.java
@@ -19,47 +19,47 @@ public class AnswerBeans implements Serializable {
 	}
 
 	//引数のないコンストラクタ(デフォルトコンストラクタ)
-		public AnswerBeans() {
-			super();
-			this.user_id = 0;
-			this.question_id = 0;
-			this.start_time = "";
-			this.user_answer = "";
-		}
+	public AnswerBeans() {
+		super();
+		this.user_id = 0;
+		this.question_id = 0;
+		this.start_time = "";
+		this.user_answer = "";
+	}
 
 	//ゲッター,セッターの生成
 
-		public int getUser_id() {
-			return user_id;
-		}
+	public int getUser_id() {
+		return user_id;
+	}
 
-		public void setUser_id(int user_id) {
-			this.user_id = user_id;
-		}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
 
-		public int getQuestion_id() {
-			return question_id;
-		}
+	public int getQuestion_id() {
+		return question_id;
+	}
 
-		public void setQuestion_id(int question_id) {
-			this.question_id = question_id;
-		}
+	public void setQuestion_id(int question_id) {
+		this.question_id = question_id;
+	}
 
-		public String getStart_time() {
-			return start_time;
-		}
+	public String getStart_time() {
+		return start_time;
+	}
 
-		public void setStart_time(String start_time) {
-			this.start_time = start_time;
-		}
+	public void setStart_time(String start_time) {
+		this.start_time = start_time;
+	}
 
-		public String getUser_answer() {
-			return user_answer;
-		}
+	public String getUser_answer() {
+		return user_answer;
+	}
 
-		public void setUser_answer(String user_answer) {
-			this.user_answer = user_answer;
-		}
+	public void setUser_answer(String user_answer) {
+		this.user_answer = user_answer;
+	}
 
 
 }
