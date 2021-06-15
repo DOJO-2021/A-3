f57cc3978d020e2c2558d96d5f588ebebb5ce93e
dojo2021-i-sasaki <sasaki-i@seplus2016.onmicrosoft.com>
2021-06-15 11:22:43 +0900

    SorceBeans.java(変更)フィールド、getter,setter、コンストラクタ追加

--- NANIKA/src/model/ScoreBeans.java
+++ NANIKA/src/model/ScoreBeans.java
@@ -1,5 +1,60 @@
 package model;
 
 public class ScoreBeans {
+	private int user_id;
+	private int unit_id;
+	private String start_time;
+	private String end_time;
+	private String score;
+	private int result;
+
+	public ScoreBeans() {
+		super();
+	}
+
+
+
+
+
+
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public int getUnit_id() {
+		return unit_id;
+	}
+	public void setUnit_id(int unit_id) {
+		this.unit_id = unit_id;
+	}
+	public String getStart_time() {
+		return start_time;
+	}
+	public void setStart_time(String start_time) {
+		this.start_time = start_time;
+	}
+	public String getEnd_time() {
+		return end_time;
+	}
+	public void setEnd_time(String end_time) {
+		this.end_time = end_time;
+	}
+	public String getScore() {
+		return score;
+	}
+	public void setScore(String score) {
+		this.score = score;
+	}
+	public int getResult() {
+		return result;
+	}
+	public void setResult(int result) {
+		this.result = result;
+	}
+
+
+
 
 }
