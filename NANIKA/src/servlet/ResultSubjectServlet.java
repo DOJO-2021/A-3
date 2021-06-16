69f4cd689df4bbcb23931c82ef55e8dcb2e4d72a
dojo2021-y-saito <saito@seplus2016.onmicrosoft.com>
2021-06-16 15:30:38 +0900

    変更

--- NANIKA/src/servlet/ResultSubjectServlet.java
+++ NANIKA/src/servlet/ResultSubjectServlet.java
@@ -17,17 +17,7 @@ import javax.servlet.http.HttpSession;
 public class ResultSubjectServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ResultSubjectServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 //		パンくずリスト用のスコープ（テスト結果一覧）
@@ -38,9 +28,6 @@ public class ResultSubjectServlet extends HttpServlet {
 		dispatcher.forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
