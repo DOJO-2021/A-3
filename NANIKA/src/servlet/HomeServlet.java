e70745afdd64a987c0955c3f4cf75221a0191732
dojo2021-m-muraoka <muraoka@seplus2016.onmicrosoft.com>
2021-06-16 10:53:20 +0900

    homesurvlet

--- NANIKA/src/servlet/HomeServlet.java
+++ NANIKA/src/servlet/HomeServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,6 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import dao.ScoreDao;
+import model.NanikaBeans;
 import model.UserBeans;
 
 /**
@@ -30,20 +33,20 @@ public class HomeServlet extends HttpServlet {
 //				return;
 			}
 
-
-
-
-
-
-
-
-
-
-
-
-			// メニューページにフォワードする
+			//Daoからスコアをとってくる
+			ScoreDao scoreDao = new ScoreDao();
+			//引数とメソッドの名前と戻り値の型を聞く　引数( nanikaBeans )userbeansGetUser_id List型返り値 radarData　scoreDao
+			ArrayList<NanikaBeans> list = (ArrayList<NanikaBeans>)scoreDao.radarData(userbeans.getUser_id());
+			//リクエスト領域にセット
+			request.setAttribute("list",list);
+			//ホームページに遷移
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/home.jsp");
 			dispatcher.forward(request, response);
+
+			// メニューページにフォワードする
+//			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/home.jsp");
+//			dispatcher.forward(request, response);
+
 		}catch(NullPointerException e) {
 			System.out.println("--------------------HomeServlet(doGet)");
 			System.out.println("ログイン画面へ戻す（userbeansがsessionに存在しない為）");
