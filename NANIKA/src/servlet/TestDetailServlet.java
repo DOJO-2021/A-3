f8a47a92134a9dcdb69180d0ccb14e722a941e20
dojo2021-lenguyen <le@seplus2016.onmicrosoft.com>
2021-06-17 14:30:11 +0900

    new

--- NANIKA/src/servlet/TestDetailServlet.java
+++ NANIKA/src/servlet/TestDetailServlet.java
@@ -1,11 +1,18 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.UserScoreDao;
+import model.Bc;
+import model.NanikaBeans;
 
 /**
  * Servlet implementation class TestDetailServlet
@@ -13,29 +20,28 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/TestDetailServlet")
 public class TestDetailServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TestDetailServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		ログイン確認処理
+//		HttpSession session = request.getSession();
+//		if (session.getAttribute("user_id") == null) {
+//			response.sendRedirect("/NANIKA/LoginServlet");
+//			return;
+//		}
+		//スコアテーブルのデータをもらう処理
+		UserScoreDao sDao = new UserScoreDao();
+		List<NanikaBeans> unitscoreList = sDao.select(new Bc(0, "", "", "", "",
+				"", "", "", "", "", "", "", "", "", null));
+		request.setAttribute("unitscoreList", unitscoreList);
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/test_detail.jsp");
+		dispatcher.forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+
 	}
 
 }
