c3217bc49eb3a9254bfe845ab5b47a89e6730f34
dojo2021-y-miyamoto <miyamoto-y@seplus2016.onmicrosoft.com>
2021-06-18 12:49:10 +0900

    unit.js 追加

---
+++ NANIKA/WebContent/js/unit.js
@@ -0,0 +1,41 @@
+$(function(){
+
+		const dc = document;
+		let unit = dc.getElementsByClassName('js-unit');
+		const menuTab = dc.getElementById('js-menuTab');
+		console.log(unit);
+		for(let i = 0; i < unit.length; i++){
+			unit[i].addEventListener("click",() => {
+
+			let requestUnit = {
+				unit_id : unit[i].name,
+				unit_name : unit[i].textContent
+			};
+			if(menuTab.textContent === "テスト受験"){
+			$.ajax({
+				type:"GET",
+				url:"http://localhost:8080/NANIKA/TestServlet",
+				data:requestUnit,
+				dateType:"json"
+			}).done(function(data){
+				console.log(data)
+			});
+
+			}
+
+			if(menuTab.textContent === "テスト結果一覧"){
+			$.ajax({
+				type:"GET",
+				url:"http://localhost:8080/NANIKA/TestDetailServlet",
+				data:requestUnit,
+				dateType:"json"
+			}).done(function(data){
+				console.log(data)
+			});
+
+
+			}
+
+			}, false);
+		}
+	});
