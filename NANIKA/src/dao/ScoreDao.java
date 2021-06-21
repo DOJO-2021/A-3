fbc4f697b8a27ab7c7890c3e4a7f4f72435a8414
dojo2021-m-muraoka <muraoka@seplus2016.onmicrosoft.com>
2021-06-21 14:10:43 +0900

    scoredao

--- NANIKA/src/dao/ScoreDao.java
+++ NANIKA/src/dao/ScoreDao.java
@@ -127,10 +127,10 @@ public class ScoreDao {
 		return scoreall;
 	}
 
-	//選択した答えをinsertする
-	public boolean insert(Nanika answer) {
+	//解答insert「table_user_score」にnsertする(user_id、question_id、stat_time、user_answer)
+	public boolean insert_table_user_score(NanikaBeans answer) {
 		Connection conn = null;
-		boolean result = false;
+		boolean user_ans = false;
 
 		try {
 			// JDBCドライバを読み込む
@@ -140,9 +140,150 @@ public class ScoreDao {
 			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/A-3/NANIKA/database", "sa", "");
 
 			// SQL文を準備する
-			String sql = "insert into 作成したテーブル values (null,null,?,?)"
+			String sql = "insert into table_user_score values (?,?,?,?)";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
 
+			//SQL文を完成させる
+			if(answer.getUser_id() != -1) {
+				pStmt.setInt(1,answer.getUser_id());
+			}
+			else {
+				pStmt.setInt(1,0);
+			}
+			if(answer.getQuestion_id() != -1) {
+				pStmt.setInt(2,answer.getQuestion_id());
+			}
+			else {
+				pStmt.setInt(2,0);
+			}
+			if(answer.getStart_time() != null) {
+				pStmt.setString(3,answer.getStart_time());
+			}
+			else {
+				pStmt.setString(3,"null");
+			}
+			if(answer.getUser_answer() != null) {
+				pStmt.setString(4,answer.getUser_answer());
+			}
+			else {
+				pStmt.setString(4,"null");
+			}
 
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				user_ans = true;
+			}
 		}
+
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally
+		{// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		// 結果を返す
+		return user_ans;
+	}
+
+
+
+
+	//解答insert「table_score」
+	//nsertする(user_id、unit_id、start_time、end_time、score、result)
+	public boolean insert_table_score(NanikaBeans answer) {
+		Connection conn = null;
+		boolean user_ans = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/A-3/NANIKA/database", "sa", "");
+
+			// SQL文を準備する
+			String sql = "insert into table_user_score values (?,?,?,?,?,?)";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			//SQL文を完成させる
+			if(answer.getUser_id() != -1) {
+				pStmt.setInt(1,answer.getUser_id());
+			}
+			else {
+				pStmt.setInt(1,0);
+			}
+			if(answer.getUnit_id() != -1) {
+				pStmt.setInt(2,answer.getUnit_id());
+			}
+			else {
+				pStmt.setInt(2,0);
+			}
+			if(answer.getStart_time() != null) {
+				pStmt.setString(3,answer.getStart_time());
+			}
+			else {
+				pStmt.setString(3,"null");
+			}
+			if(answer.getEnd_time()!= null) {
+				pStmt.setString(4,answer.getEnd_time());
+			}
+			else {
+				pStmt.setString(4,"null");
+			}
+
+			if(answer.getScore() != -1) {
+				pStmt.setInt(5,answer.getScore());
+			}
+			else {
+				pStmt.setInt(5,0);
+			}
+			if(answer.getResult() != -1) {
+				pStmt.setInt(6,answer.getResult());
+			}
+			else {
+				pStmt.setInt(6,0);
+			}
+
+
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				user_ans = true;
+			}
+		}
+
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally
+		{// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		// 結果を返す
+		return user_ans;
+	}
+
+
+
 
 }
