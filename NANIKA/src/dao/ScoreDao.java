f5f3864839f521b3091c68b30ab7510a64448b84
dojo2021-y-miyamoto <miyamoto-y@seplus2016.onmicrosoft.com>
2021-06-24 11:34:37 +0900

    aergrgar

--- NANIKA/src/dao/ScoreDao.java
+++ NANIKA/src/dao/ScoreDao.java
@@ -24,14 +24,16 @@ public class ScoreDao {
 			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/A-3/NANIKA/database", "sa", "");
 
 			// SELECT文を準備する
-			String sql = "select s.user_id , sub.subject, avg(score), sub.subject_id from table_score  s inner join  table_unit  u on  s.unit_id = u.unit_id inner join  table_subject  sub  on  u.subject_id = sub.subject_id where user_id = ? and (u.unit_id,end_time) in (select unit_id, max(end_time) from table_score group by unit_id) group by  s.user_id ,  sub.subject";
+			String sql = "select distinct isnull(s.user_id, ?) as user_id , sub.subject, avg(score), sub.subject_id from table_subject  sub left join table_unit u on u.subject_id = sub.subject_id left join table_score s on s.unit_id = u.unit_id where user_id = ? or user_id is null group by s.user_id, sub.subject order by subject_id asc,  avg(score) desc ";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 			pStmt.setInt(1, user_id);
+			pStmt.setInt(2, user_id);
 
 			// SELECT文を実行し、結果表を取得する
 			ResultSet rs = pStmt.executeQuery();
 
 			// 結果表をコレクションにコピーする
+			int aa =0;
 			while (rs.next()) {
 				NanikaBeans score = new NanikaBeans(
 				rs.getInt("user_id"),
@@ -39,8 +41,15 @@ public class ScoreDao {
 				rs.getInt("avg(score)"),
 				rs.getInt("subject_id")
 				);
+				if(score.getSubject_id() == aa) {
+					continue;
+				}
 				radarList.add(score);
-		}
+
+			 aa = rs.getInt("subject_id");
+
+			}
+
 		}
 		catch (SQLException e) {
 			e.printStackTrace();
@@ -65,7 +74,6 @@ public class ScoreDao {
 		// 結果を返す
 		return radarList;
 	}
-
 
 
 	//1.4テスト詳細画面用
