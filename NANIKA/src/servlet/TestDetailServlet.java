699eef73eb44422e2f5b2aca78cde60be7aafaea
dojo2021-y-miyamoto <miyamoto-y@seplus2016.onmicrosoft.com>
2021-06-24 09:49:35 +0900

    safefeqfwe

--- NANIKA/src/servlet/TestDetailServlet.java
+++ NANIKA/src/servlet/TestDetailServlet.java
@@ -33,9 +33,12 @@ public class TestDetailServlet extends HttpServlet {
 			session.setAttribute("unitId", unitId);
 		}
 
-		//		ログイン確認処理
 		HttpSession session = request.getSession();
 		UserBeans userbeans = (UserBeans) session.getAttribute("userbeans");
+
+		try {
+		//		ログイン確認処理
+
 		int unitId = Integer.parseInt((String)session.getAttribute("unitId"));
 		int userId = userbeans.getUser_id();
 		request.setAttribute("unit_id", unitId);
@@ -52,8 +55,28 @@ public class TestDetailServlet extends HttpServlet {
 			System.out.println();
 		}
 
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/test_detail.jsp");
-		dispatcher.forward(request, response);
+
+
+		}catch(ClassCastException e) {
+			int unitId = (int)session.getAttribute("unitId");
+			int userId = userbeans.getUser_id();
+			request.setAttribute("unit_id", unitId);
+
+			//スコアテーブルのデータをもらう処理
+			ScoreDao sDao = new ScoreDao();
+			List<NanikaBeans> scoreall = sDao.scoreAll(userId, unitId);
+
+			request.setAttribute("scoreall", scoreall);
+			for (NanikaBeans nanika : scoreall) {
+				System.out.println("----------------ResultServlet");
+				System.out.println("ユーザID:"+ nanika.getUser_id());
+				System.out.println("テスト科目:"+ nanika.getUnit());
+				System.out.println();
+			}
+		}finally{
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/test_detail.jsp");
+			dispatcher.forward(request, response);
+		}
 	}
 
 }
